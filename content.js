deb4abe8f37648ca0c23003f015a0e71ea5b3c04
EatYourSoup7 <r.kondratowicz1@gmail.com>
2020-02-24 13:59:28 -0600

    working off video tutorial to create manifest and popups

---
+++ content.js
@@ -0,0 +1,17 @@
+
+
+chrome.runtime.onMessage.addListener(gotMessage);
+
+function gotMessage(message, sender, sendResponse) {
+  console.log(message.txt);
+  if (message.txt === "what up") {
+    console.log("What up I'm a Chrome Extension")
+
+    let paragraphs = document.getElementsByTagName('p');
+    for (elt of paragraphs) {
+
+      elt.style['background-color'] = 'black'
+    }
+  }
+}
+
